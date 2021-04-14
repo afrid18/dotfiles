ecc240ec20f769a85f3d41966a01654da1bb0442
afrid83 <afridcool83@gmail.com>
2021-04-14 10:30:09 +0530

    my initial configurations

---
+++ .pyconfig.py
@@ -0,0 +1,15 @@
+def sysf(command: str):
+    from os import system
+    system(command)
+
+
+# variables list
+
+
+vim = 'vim'
+ls = 'ls'
+cd = 'cd'
+clear = 'clear'
+sudo = 'sudo'
+history = 'history'
+
