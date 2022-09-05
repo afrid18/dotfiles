599d8048066e667e9c05aa654e397fc2201b0bf1
Afrid Hussain <afridhussain.d@gmail.com>
2022-09-05 15:58:00 +0530

    Started to use helix editor and added config file

--- stow_script.sh
+++ stow_script.sh
@@ -1,4 +1,4 @@
 echo "This is a stow script"
-stow -R alacritty bin nvim starship tmux tmuxinator vim zsh git --verbose=1
+stow -R alacritty bin nvim starship tmux tmuxinator vim zsh git helix --verbose=1
 echo "Successfully stowed----------->"
 
