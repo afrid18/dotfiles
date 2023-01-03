fb3197b1fa03a5f420e501c5e492501976aaa738
Afrid Hussain <afridhussain.d@gmail.com>
2023-01-03 20:24:48 +0530

    updated stow_script

--- stow_script.sh
+++ stow_script.sh
@@ -1,4 +1,5 @@
 echo "This is a stow script"
 stow -R alacritty bin nvim starship tmux tmuxinator vim git helix --verbose=1
+cat ~/dotfiles/zsh/.zshrc >> ~/.zshrc
 echo "Successfully stowed----------->"
 
