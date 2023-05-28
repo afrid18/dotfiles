14808b66c31e710e2c34e3d3fe7f6913ef2e6230
Afrid Hussain <afridhussain.d@gmail.com>
2023-05-28 19:15:22 +0530

    Added nvim to stow_script.sh

--- stow_script.sh
+++ stow_script.sh
@@ -1,5 +1,5 @@
 echo "Setting up MacOS for Afrid Hussain"
 echo "This is a stow script"
-stow -R alacritty bin starship tmux tmuxinator vim git helix zsh fish --verbose=1
+stow -R alacritty bin starship tmux tmuxinator vim git helix zsh fish nvim --verbose=1
 echo "Successfully stowed----------->"
 
