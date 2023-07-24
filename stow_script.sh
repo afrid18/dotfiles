698a2feecbb902145c41729b12cc2fccae3d6853
afrid18 <afridhussain.d@gmail.com>
2023-07-24 20:53:43 +0530

    stow_script: remove tmuxinator and helix from stowing

--- stow_script.sh
+++ stow_script.sh
@@ -14,6 +14,6 @@ else
     curl -L git.io/antigen > $HOME/.config/zsh/antigen.zsh
 fi
 
-stow -R alacritty starship tmux tmuxinator vim git helix zsh nvim --verbose=1
+stow -R alacritty starship tmux vim git zsh nvim --verbose=1 --target=$HOME
 echo "Successfully stowed----------->"
 
