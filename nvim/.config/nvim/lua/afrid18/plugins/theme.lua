60dd6e2a3ec156ad06cbbabadd3afc21a430e466
afrid18 <afridhussain.d@gmail.com>
2024-02-13 21:10:02 +0530

    nvim: change theme to leaf

--- nvim/.config/nvim/lua/afrid18/plugins/theme.lua
+++ nvim/.config/nvim/lua/afrid18/plugins/theme.lua
@@ -1,20 +1,26 @@
+-- return {
+--   "CrispyBaccoon/evergarden",
+--   -- opts = {
+--   --   transparent_background = true,
+--   --   contrast_dark = 'soft', -- 'hard'|'medium'|'soft'
+--   --   overrides = {},         -- add custom overrides
+--   -- },
+--   --
+--   config = function()
+--     vim.cmd [[
+--       set termguicolors
+--       set background=dark
+--       colorscheme evergarden
+--       highlight Normal guibg=#171717
+--       highlight SignColumn guibg=#171717
+--       highlight LineNr guibg=#171717
+--       highlight Search guibg=#575454
+--     ]]
+--   end,
+-- }
+--
+--
+
 return {
-  "CrispyBaccoon/evergarden",
-  -- opts = {
-  --   transparent_background = true,
-  --   contrast_dark = 'soft', -- 'hard'|'medium'|'soft'
-  --   overrides = {},         -- add custom overrides
-  -- },
-  --
-  config = function()
-    vim.cmd [[
-      set termguicolors
-      set background=dark
-      colorscheme evergarden
-      highlight Normal guibg=#171717
-      highlight SignColumn guibg=#171717
-      highlight LineNr guibg=#171717
-      highlight Search guibg=#575454
-    ]]
-  end,
+  "daschw/leaf.nvim"
 }
