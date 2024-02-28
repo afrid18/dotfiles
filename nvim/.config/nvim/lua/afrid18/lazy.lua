6b62430d1a7cfaeb132120c28f013d22e1643bbc
afrid18 <afridhussain.d@gmail.com>
2024-02-28 10:21:49 -0500

    format: lua

--- nvim/.config/nvim/lua/afrid18/lazy.lua
+++ nvim/.config/nvim/lua/afrid18/lazy.lua
@@ -12,6 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { import = "afrid18.plugins"}, 
-  { import = "afrid18.plugins.lsp"} 
+  { import = "afrid18.plugins" },
+  { import = "afrid18.plugins.lsp" }
 })
