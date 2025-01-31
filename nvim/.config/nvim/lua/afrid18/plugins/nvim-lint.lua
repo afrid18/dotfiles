c7849ae27aba1f0c0c5680370bd85e214fe7963c
afrid18 <afridhussain.d@gmail.com>
2025-02-01 03:49:55 +0530

    latest changes + raycast config

--- nvim/.config/nvim/lua/afrid18/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/afrid18/plugins/nvim-lint.lua
@@ -10,7 +10,7 @@ return {
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
-      python = { "flake8" },
+      python = { "ruff" },
       cpp = { "cpplint" },
       -- java = { "checkstyle" },
     }
