18f9ab7f3eb9e301346e13f32ba69be996232dd6
afrid18 <afridhussain.d@gmail.com>
2026-08-03 21:31:40 +0530

    ghostty: add ghostty config

--- stow_script.sh
+++ stow_script.sh
@@ -1,5 +1,5 @@
 echo "Setting up MacOS for Afrid Hussain"
 echo "This is a stow script"
-stow -R kitty alacritty starship tmux vim git zsh nvim --verbose=1 --target=$HOME
+stow -R kitty alacritty starship tmux vim git ghostty zsh nvim --verbose=1 --target=$HOME
 echo "Successfully stowed----------->"
 
